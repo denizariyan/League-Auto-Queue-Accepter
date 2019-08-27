3c482db8a4a82a7e3060860faeccc2da2868debb
Deniz Ariyan <30367397+denizariyan@users.noreply.github.com>
2019-08-27 23:10:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import pyautogui
+
+active = True
+while active:
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread("template.png", cv2.IMREAD_GRAYSCALE)
+    result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+    location = np.where(result >= 0.9)
+    if location[0].any():
+        active = False
+location = np.where(result >= 0.9)
+w, h = template.shape[::-1]
+
+for pt in zip(*location[::-1]):
+    cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+# cv2.imshow("image", image)
+
+pyautogui.click(location[1][0], location[0][0])
+# cv2.imshow("result", result)
