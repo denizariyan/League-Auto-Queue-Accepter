b03a942d5075bbb1c625b9b17f9128f7b5ec37f7
Deniz Ariyan <30367397+denizariyan@users.noreply.github.com>
2020-10-18 01:29:47 +0300

    Fix hogging of CPU resources
    Added sleep to not hog all the CPU resources

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import cv2
 import numpy as np
 import pyautogui
+import time
+
+template = cv2.imread("template.png", cv2.IMREAD_GRAYSCALE)
 
 active = True
 while active:
     image = pyautogui.screenshot()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread("template.png", cv2.IMREAD_GRAYSCALE)
     result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
     location = np.where(result >= 0.9)
+    time.sleep(2) # To not hog all the CPU resources while searching
     if location[0].any():
         active = False
 location = np.where(result >= 0.9)
